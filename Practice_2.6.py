ea35efb129669b97e07bac7f8e57a6f90bb3971b
Fatima Ehtesham <77339740+Fatima-Ehtesham@users.noreply.github.com>
2022-01-11 19:53:51 +0500

    Delete Practice_2.6.py

--- Practice_2.6.py
+++
@@ -1,7 +0,0 @@
-# Q) words = ['bat','ball','barn','basket','badminton']
-# write two python expression that evaluates to first and 
-# last, respectively, words in words, in dictionary order? 
-
-words = ['bat','ball','barn','basket','badminton']
-print(min(words))
-print(max(words))
